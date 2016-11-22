005c7c0d767768255d3019ba01c9b51ae145b80a
acasolla <casolla.alessandro@gmail.com>
2016-11-22 18:01:55 +0100

    init

--- app/src/main/java/com/example/ale/keychain/DisplayMessageActivity.java
+++ app/src/main/java/com/example/ale/keychain/DisplayMessageActivity.java
@@ -5,6 +5,7 @@ import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.logging.Logger;
@@ -16,15 +17,20 @@ public class DisplayMessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
-
+    //gets data from intent
         Intent i = getIntent();
         String message = i.getStringExtra(MainActivity.EXTRA_MESSAGE);
         TextView tv = new TextView(this);
         tv.setTextSize(40);
         tv.setText(message);
 
+        //Adds the tv to the layout
         ViewGroup layout = (ViewGroup) findViewById(R.id.activity_display_message);
         layout.addView(tv);
+
+        TextView detail = (TextView) findViewById(R.id.detail_title);
+        detail.setText(R.string.detail_activity);
+
     }
 
     @Override
