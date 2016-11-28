dc698b5a863f81f2d198fe1eef5a341bf29701ff
acasolla <casolla.alessandro@gmail.com>
2016-11-28 14:41:05 +0100

    init

--- app/src/main/java/com/example/ale/keychain/MainActivity.java
+++ app/src/main/java/com/example/ale/keychain/MainActivity.java
@@ -22,67 +22,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void sendMessage(View view ){
-        Intent i = new Intent(this,DisplayMessageActivity.class);
-        EditText et = (EditText) findViewById(R.id.edit_message);
-        String message = et.getText().toString();
-        logger.info("message=" + et.getText());
-        i.putExtra(EXTRA_MESSAGE,message);
-        startActivity(i);
-        logger.info("starting activity");
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        logger.info("onStart");
-    }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        logger.info("onStop");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        logger.info("onDestroy");
-    }
-
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-        logger.info("onBackPressed");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        logger.info("onPause");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        logger.info("onResume");
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
-        logger.info("onCreate");
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        logger.info("onSaveInstanceState");
-    }
-
-    @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-        logger.info("onRestoreInstanceState");
-
-    }
 }
