131d92d1e36c719618b2cd46af048e68dfdc0927
acasolla <casolla.alessandro@gmail.com>
2016-12-01 13:55:02 +0100

    IND

--- app/src/main/java/com/example/ale/keychain/NetworkingActivity.java
+++ app/src/main/java/com/example/ale/keychain/NetworkingActivity.java
@@ -7,10 +7,7 @@ import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.JsonReader;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -26,8 +23,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.Reader;
-import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
@@ -36,57 +31,80 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class NetworkingActivity extends AppCompatActivity {
+
     private final static String TAG = "NetworkingActivity";
-    private final static String REST_URL = "http://services.groupkt.com/state/get/USA/all";
+    private final static String REST_URL_USA = "http://services.groupkt.com/state/get/USA/all";
+    private final static String REST_URL_IND = "http://services.groupkt.com/state/get/IND/all";
     TextView textView;
     ListView listView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_networking);
-        textView = (TextView) findViewById(R.id.tv);
-        listView = (ListView) findViewById(R.id.list);
+        listView = (ListView)findViewById(R.id.list);
     }
 
 
-
-
-    public void performCall( View view ){
+    public void performCall(View view) {
         ConnectivityManager connMgr = (ConnectivityManager)
                 getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
         if (networkInfo != null && networkInfo.isConnected()) {
-            new CallRestTask().execute(REST_URL);
+            new CallRestTask().execute(REST_URL_USA,REST_URL_IND);
         } else {
-            Toast.makeText(this,"You are offline",Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "You are offline", Toast.LENGTH_SHORT).show();
         }
+
     }
-    private class CallRestTask extends AsyncTask<String,Void,String> {
+
+
+
+    private class CallRestTask extends AsyncTask<String, String, String> {
         private ProgressDialog progressDialog = new ProgressDialog(NetworkingActivity.this);
+
         protected void onPreExecute() {
-            progressDialog.setMessage("Please wait..");
+            progressDialog.setMessage("Calling ws..");
             progressDialog.show();
         }
+
+
         @Override
-        protected String doInBackground(String... params) {
+        protected String doInBackground(String... urls) {
             try {
-                return makeServiceCall(params[0]);
+                String result = null;
+                for ( String url :urls ){
+                    result = makeServiceCall(url);
+                    publishProgress("Parsed url:" + url);
+                    Thread.sleep(1000);
+                }
+                return result;
             } catch (Exception e) {
                 return "Unable to retrieve web page. URL may be invalid.";
             }
         }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            progressDialog.setMessage(values[0]);
+        }
+
         @Override
         protected void onPostExecute(String result) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             progressDialog.dismiss();
-           // textView.setText(result);
             try {
                 JSONObject object = new JSONObject(result);
-                Log.d(TAG,"response json=" + object);
+                Log.d(TAG, "response json=" + object);
                 JSONObject restResponse = object.getJSONObject("RestResponse");
                 JSONArray arr = restResponse.getJSONArray("result");
                 List<String> values = new ArrayList<String>(arr.length());
 
-                for ( int i = 0; i < arr.length(); i++ ){
+                for (int i = 0; i < arr.length(); i++) {
                     JSONObject child = (JSONObject) arr.get(i);
                     String name = child.getString("name");
                     values.add("Name : " + name);
@@ -98,6 +116,8 @@ public class NetworkingActivity extends AppCompatActivity {
             } catch (JSONException e) {
                 e.printStackTrace();
             }
+
+
         }
 
 
