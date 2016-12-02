e3501b6afb5149d0ea5c94d1779580a507e70ca1
acasolla <casolla.alessandro@gmail.com>
2016-12-02 16:45:59 +0100

    maps

--- app/src/main/java/com/example/ale/keychain/MainActivity.java
+++ app/src/main/java/com/example/ale/keychain/MainActivity.java
@@ -1,34 +1,13 @@
 package com.example.ale.keychain;
 
-import android.app.ListActivity;
-import android.content.ContentValues;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.speech.tts.TextToSpeech;
-import android.support.v4.app.NavUtils;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.GridView;
-import android.widget.ImageView;
-import android.widget.SimpleCursorAdapter;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.ale.keychain.adapter.GridAdapter;
-import com.example.ale.keychain.sql.PasswordContract;
-import com.example.ale.keychain.sql.PasswordDbHelper;
 
 import java.util.logging.Logger;
 
@@ -38,11 +17,12 @@ public class MainActivity extends AppCompatActivity {
 
 
     private GridView gridView;
-    static final String[] names = new String[] {"Camera", "Download", "Keychain", "Networking"};
+    static final String[] names = new String[] {"Camera", "Download", "Keychain", "Networking","Maps","Location"};
     static final int[] images = new int[]{R.mipmap.ic_photo_camera_black_48dp,
                                           R.mipmap.ic_file_download_black_48dp,
                                           R.mipmap.ic_vpn_key_black_48dp,
-                                          R.mipmap.ic_settings_ethernet_black_48dp};
+                                          R.mipmap.ic_settings_ethernet_black_48dp,
+    R.mipmap.ic_place_black_48dp,R.mipmap.ic_my_location_black_48dp};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -72,6 +52,14 @@ public class MainActivity extends AppCompatActivity {
             case R.mipmap.ic_vpn_key_black_48dp:
                 Log.d("TAG","Clicked camera");
                 i = new Intent(this,LoginActivity.class);
+                break;
+            case R.mipmap.ic_place_black_48dp:
+                Log.d("TAG","Clicked camera");
+                i = new Intent(this,MapsActivity.class);
+                break;
+            case R.mipmap.ic_my_location_black_48dp:
+                Log.d("TAG","Clicked camera");
+                i = new Intent(this,LocationActivity.class);
                 break;
 
 
---
+++ app/src/main/java/com/example/ale/keychain/MapsActivity.java
@@ -0,0 +1,135 @@
+package com.example.ale.keychain;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private LocationManager locationMangaer = null;
+    private android.location.LocationListener locationListener = null;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        locationMangaer = (LocationManager)
+                getSystemService(Context.LOCATION_SERVICE);
+
+        locationListener = new MapsActivity.MyLocationListener();
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationMangaer.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 5000, 10, locationListener);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    /*----------Listener class to get coordinates ------------- */
+    private class MyLocationListener implements android.location.LocationListener {
+        @Override
+        public void onLocationChanged(Location loc) {
+
+            String longitude = "Longitude: " + loc.getLongitude();
+            Log.v("TAG", longitude);
+            String latitude = "Latitude: " + loc.getLatitude();
+            Log.v("TAG", latitude);
+
+            LatLng currentPosition = new LatLng(loc.getLatitude(), loc.getLongitude());
+
+            mMap.addMarker(new MarkerOptions().position(currentPosition));
+            Toast.makeText(MapsActivity.this, "Marker showed", Toast.LENGTH_LONG).show();
+
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentPosition, 15));
+            // Zoom in, animating the camera.
+            mMap.animateCamera(CameraUpdateFactory.zoomIn());
+            // Zoom out to zoom level 10, animating with a duration of 2 seconds.
+            mMap.animateCamera(CameraUpdateFactory.zoomTo(15), 2000, null);
+
+            if (ActivityCompat.checkSelfPermission(MapsActivity.this,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                    ActivityCompat.checkSelfPermission(MapsActivity.this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            locationMangaer.removeUpdates(this);
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            // TODO Auto-generated method stub
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            // TODO Auto-generated method stub
+        }
+
+        @Override
+        public void onStatusChanged(String provider,
+                                    int status, Bundle extras) {
+            // TODO Auto-generated method stub
+        }
+    }
+}
